f6e39057c60494a3b1f44b84de66d6f60dfe93c6
Chaosoffire <81634128+chaosoffire@users.noreply.github.com>
2026-01-07 01:17:58 +0800

    customize changes
    Signed-off-by: Chaosoffire <81634128+chaosoffire@users.noreply.github.com>

--- utils.sh
+++ utils.sh
@@ -460,7 +460,7 @@ get_archive_pkg_name() { echo "$__ARCHIVE_PKG_NAME__"; }
 patch_apk() {
 	local stock_input=$1 patched_apk=$2 patcher_args=$3 cli_jar=$4 patches_jar=$5
 	local cmd="env -u GITHUB_REPOSITORY java -jar $cli_jar patch $stock_input --purge -o $patched_apk -p $patches_jar --keystore=ks.keystore \
---keystore-entry-password=123456789 --keystore-password=123456789 --signer=jhc --keystore-entry-alias=jhc $patcher_args"
+--keystore-entry-password=${KEYSTORE_PASSWORD:-123456789} --keystore-password=${KEYSTORE_PASSWORD:-123456789} --signer=${KEYSTORE_ALIAS:-jhc} --keystore-entry-alias=${KEYSTORE_ALIAS:-jhc} $patcher_args"
 	if [ "$OS" = Android ]; then cmd+=" --custom-aapt2-binary=${AAPT2}"; fi
 	pr "$cmd"
 	if eval "$cmd"; then [ -f "$patched_apk" ]; else
